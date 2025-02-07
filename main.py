3f577e812c041749b9c245be013108697fba6c25
Aphrale <rowloch@gmail.com>
2025-02-07 20:05:10 +0000

    added untracked files

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ print (f"Screen height: {SCREEN_HEIGHT}")
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 while True:
-    pygame.surface.fill(0, 0, 0)
+    pygame.Surface.fill(screen, (0,0,0))
     pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
 if __name__ == "__main__":
     main()
 
